42caad3c691c0edf6b4f5d03c6bd72a5e829da82
MacStudent <macstudent@126-Instructor.local>
2019-03-19 21:08:09 -0400

    Project Setup

---
+++ 2019W_MAD_3115_MidTerm/Model/Student.swift
@@ -0,0 +1,9 @@
+//
+//  Student.swift
+//  2019W_MAD_3115_MidTerm
+//
+//  Created by MacStudent on 2019-03-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
---
+++ 2019W_MAD_3115_MidTerm/Students/StudentResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StudentResultViewController.swift
+//  2019W_MAD_3115_MidTerm
+//
+//  Created by MacStudent on 2019-03-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class StudentResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
